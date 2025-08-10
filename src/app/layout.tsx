83e868726fdba4854ec7c2c05ced47cb7398d20e
Ibnu Wibowo <ibnuw83@gmail.com>
2025-08-10 10:06:38 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Toko Cepat',
   description: 'Aplikasi Point of Sale (POS) modern dan cepat',
-  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,7 +18,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
         {children}
