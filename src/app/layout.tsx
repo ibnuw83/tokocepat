5d27f3d3469efa00d3d1cebbc57cd876a799a919
Ibnu Wibowo <ibnuw83@gmail.com>
2025-08-10 12:44:12 +0000

    cara deploy kemana aplikasi ini biar dapat di isntal di hp andorid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Toko Cepat',
   description: 'Aplikasi Point of Sale (POS) modern dan cepat',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#FFFFFF" />
       </head>
       <body className="font-body antialiased">
         {children}
